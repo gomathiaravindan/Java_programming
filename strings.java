ddc397fa4309b1b78f2e5a997b51315b23cb3664
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-01-21 14:41:57 +0530

    Create strings.java

---
+++ strings.java
@@ -0,0 +1,16 @@
+public class strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String txt = "Gomathi";
+		int var = 89;
+		double val = 90.98;
+		System.out.println(txt);
+		
+		System.out.println("Hello "+ txt);
+		String concat = "Hello " + txt;
+		System.out.println(concat);
+		System.out.println("The values are: "+ var +","+ val+ "," + txt);
+	}
+
+}
