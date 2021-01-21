63d5289b27afebb2b12f3767df3155e153cc3a3c
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-01-21 14:41:10 +0530

    Create datatypes_variables.java

---
+++ datatypes_variables.java
@@ -0,0 +1,24 @@
+public class variable {
+	
+	public static void main(String[] args)
+	{
+		int var = 45;
+		short shrt = 34;
+		long lng = 56;
+				
+		double dble = 9.0834;
+		
+		float flt = 9.345f;
+		char ch = 'A';
+		boolean bool = true;
+		byte abyt = 123;
+		System.out.println(var);
+		System.out.println(shrt);
+		System.out.println(lng);
+		System.out.println(dble);
+		System.out.println(ch);
+		System.out.println(flt);
+		System.out.println(bool);
+		System.out.println(abyt);
+	}
+}
