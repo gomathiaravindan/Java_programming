bfcaf80fb9f6306da0311146959eef81ffc102e3
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-01-21 14:40:22 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class he_wo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.print("I am Gomathi");
+	}
+
+}
