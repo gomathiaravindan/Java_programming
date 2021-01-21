5337eeb6fe55797dab1ea79356f1619014902b50
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-01-21 14:46:08 +0530

    Create if_statements.java

---
+++ if_statements.java
@@ -0,0 +1,26 @@
+public class if_stataments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+		if(n!= 100)
+			System.out.println("Yes!!");
+		else if(n == 10)
+			System.out.println("Yes Yes!!");
+		else
+			System.out.println("No!!");
+		
+		int num = 5;
+		while(num < 10)
+		{
+			
+			System.out.println("Thank you!!"+num);
+			if(num==7)
+				break;
+			
+			num++;
+		}
+				
+	}
+
+}
